a86603fc5a78f6e4ee9009dc301f618a41181977
Walter Indeche <walter.indeche@andela.com>
2022-07-28 11:30:22 +0300

    Added Swiperefresh and delete item

--- app/src/main/java/com/walter/sqlite/DisplayActivity.kt
+++ app/src/main/java/com/walter/sqlite/DisplayActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,7 @@ class DisplayActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display)
 
         val rvProducts : RecyclerView = findViewById(R.id.recyclerViewProducts)
+        val swipeRefresh: SwipeRefreshLayout = findViewById(R.id.swipeLayout)
 
         val db = DBHelper(this, null)
 
@@ -33,6 +35,22 @@ class DisplayActivity : AppCompatActivity() {
         val adapter = CustomAdapter(productsList)
 
         rvProducts.adapter = adapter
+
+        swipeRefresh.setOnRefreshListener {
+                productsList.clear()
+                val cursor = db.getProducts()
+                //select * from products
+                while (cursor!!.moveToNext()){
+                    val id = cursor.getInt(0)
+                    val name = cursor.getString(1)
+                    val quantity = cursor.getInt(2)
+                    val price = cursor.getInt(3)
+                    val product = Product(id, name, quantity, price)
+                    productsList.add(product)
+                }
+                adapter.notifyDataSetChanged()
+                swipeRefresh.isRefreshing = false
+        }
 
     }
 }
--- app/src/main/java/com/walter/sqlite/UpdateActivity.kt
+++ app/src/main/java/com/walter/sqlite/UpdateActivity.kt
@@ -5,18 +5,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class UpdateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update)
 
-        val inputEditName : EditText = findViewById(R.id.inputEditName)
-        val inputEditQuantity : EditText = findViewById(R.id.inputEditQuantity)
-        val inputEditPrice : EditText = findViewById(R.id.inputEditPrice)
-        val buttonUpdate : Button = findViewById(R.id.buttonEditSave)
+        val inputEditName: EditText = findViewById(R.id.inputEditName)
+        val inputEditQuantity: EditText = findViewById(R.id.inputEditQuantity)
+        val inputEditPrice: EditText = findViewById(R.id.inputEditPrice)
+        val buttonUpdate: Button = findViewById(R.id.buttonEditSave)
+        val buttonDelete: Button = findViewById(R.id.buttonDeleteItem)
 
-        val id  = intent.getIntExtra("id", 0)
+        val id = intent.getIntExtra("id", 0)
         val db = DBHelper(this, null)
         val cursor = db.fetchOneProduct(id)
         cursor!!.moveToFirst()
@@ -31,19 +33,35 @@ class UpdateActivity : AppCompatActivity() {
             val quantity = inputEditQuantity.text.toString().trim().toIntOrNull()
             val price = inputEditPrice.text.toString().trim().toIntOrNull()
 
-            if (name.isNotEmpty() && quantity != null && price !=null){
+            if (name.isNotEmpty() && quantity != null && price != null) {
                 db.updateProduct(id, name, quantity, price)
                 Toast.makeText(this, "Product $name has been updated", Toast.LENGTH_SHORT).show()
                 //navigate back
                 finish() //kill the current activity
-            }else{
+            } else {
                 Toast.makeText(this, "Please enter valid values", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        buttonDelete.setOnClickListener {
+            // db.deleteProduct(id)
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Delete Product")
+            builder.setMessage("Do you really want to delete this item?")
+            builder.setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            builder.setNegativeButton("Delete") { _, _ ->
+                db.deleteProduct(id)
+                Toast.makeText(this, "Item Deleted", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            builder.show()
         }
     }
 
     //We will finish search and delete
-
     //git add .
     //git commit -m "Added display activity"
     //git push origin master
